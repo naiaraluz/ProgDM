c3b862d9fbc094682ba96d75fa71dd3deb6e0756
MarcosGF-developer <marcos.g01@aluno.ifsc.edu.br>
2021-11-09 22:31:21 -0300

    Relacionamento Categoria Finalizado

---
+++ PDM-main/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:projeto_banco/ui/chamado_page.dart';
+import 'package:projeto_banco/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
